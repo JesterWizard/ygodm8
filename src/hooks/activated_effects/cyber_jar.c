79e56f51131a48908df01d90b2e1ca872d076fc5
JesterWizard <adapps101@gmail.com>
2026-05-14 08:13:02 +0100

    Effect - Cyber Jar

--- src/hooks/activated_effects/cyber_jar.c
+++ src/hooks/activated_effects/cyber_jar.c
@@ -18,7 +18,9 @@ void __attribute__((section(".append_text"))) EffectCyberJar(void)
     for (i = 0; i < 2; i++)
     {
         u8 turn = (i == 0) ? activeTurn : inactiveTurn;
+        struct DuelCard **monsterRow = (i == 0) ? gTurnZones[2] : gTurnZones[1];
         u8 handSize = 0;
+        u8 drawn = 0;
 
         for (k = 0; k < MAX_ZONES_IN_ROW; k++)
             if (gDuel.hands[turn][k].id != CARD_NONE)
@@ -27,6 +29,7 @@ void __attribute__((section(".append_text"))) EffectCyberJar(void)
         while (handSize < 5)
         {
             u16 drawnCard;
+            s8 emptyZone;
 
             if (gDuelDecks[turn].cardsDrawn >= NumCardsInDeck(turn))
             {
@@ -36,13 +39,36 @@ void __attribute__((section(".append_text"))) EffectCyberJar(void)
             drawnCard = gDuelDecks[turn].cards[gDuelDecks[turn].cardsDrawn];
             gDuelDecks[turn].cardsDrawn++;
 
-            for (k = 0; k < MAX_ZONES_IN_ROW; k++)
-                if (gDuel.hands[turn][k].id == CARD_NONE)
-                {
-                    gDuel.hands[turn][k].id = drawnCard;
-                    handSize++;
-                    break;
-                }
+            SetCardInfo(drawnCard);
+            emptyZone = FirstEmptyZoneInRow(monsterRow);
+
+            if (GetTypeGroup(drawnCard) == TYPE_GROUP_MONSTER
+                && gCardInfo.level >= 1 && gCardInfo.level <= 4
+                && NumEmptyZonesInRow(monsterRow) > 0)
+            {
+                monsterRow[emptyZone]->id = drawnCard;
+                monsterRow[emptyZone]->isFaceUp = TRUE;
+                monsterRow[emptyZone]->isLocked = TRUE;
+                monsterRow[emptyZone]->isDefending = FALSE;
+                monsterRow[emptyZone]->permStage = 0;
+                monsterRow[emptyZone]->tempStage = 0;
+                monsterRow[emptyZone]->unk4 = 0;
+                monsterRow[emptyZone]->unkTwo = 0;
+                monsterRow[emptyZone]->willChangeSides = 0;
+            }
+            else
+            {
+                for (k = 0; k < MAX_ZONES_IN_ROW; k++)
+                    if (gDuel.hands[turn][k].id == CARD_NONE)
+                    {
+                        gDuel.hands[turn][k].id = drawnCard;
+                        handSize++;
+                        break;
+                    }
+            }
+            drawn++;
+            if (drawn >= 10)
+                break;
         }
     }
 
